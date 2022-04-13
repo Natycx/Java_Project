03246913880a1e2db3d0b8081875d8327107b115
Natycx <talia_garcia@hotmail.com>
2022-04-12 22:13:24 -0300

    Continuação

---
+++ src/Secao_17/entities/UsaInterestService.java
@@ -0,0 +1,17 @@
+package Secao_17.entities;
+
+import java.security.InvalidParameterException;
+
+public class UsaInterestService implements InterestService{
+
+    private double interestRate;
+
+    public UsaInterestService(double interestRate) {
+        this.interestRate = interestRate;
+    }
+    @Override
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+}
